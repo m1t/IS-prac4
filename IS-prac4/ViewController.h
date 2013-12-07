c86946a4b976d59a959cd6034cddec1184cc844e
mits <mits.jp@gmail.com>
2013-12-07 16:06:51 +0900

    Initial Commit

---
+++ IS-prac4/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  IS-prac4
+//
+//  Created by Chida Mitsuhiro on 13/12/07.
+//  Copyright (c) 2013年 Chida Mitsuhiro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ IS-prac4Tests/IS_prac4Tests.m
@@ -0,0 +1,32 @@
+//
+//  IS_prac4Tests.m
+//  IS-prac4Tests
+//
+//  Created by Chida Mitsuhiro on 13/12/07.
+//  Copyright (c) 2013年 Chida Mitsuhiro. All rights reserved.
+//
+
+#import "IS_prac4Tests.h"
+
+@implementation IS_prac4Tests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in IS-prac4Tests");
+}
+
+@end
